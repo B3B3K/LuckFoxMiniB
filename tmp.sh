c317cf6669d38d52053d316e7aa128960e38cd4e
Osman Çelik <76453193+B3B3K@users.noreply.github.com>
2025-01-21 19:53:37 +0300

    fixes

--- tmp.sh
+++ tmp.sh
@@ -2,4 +2,5 @@ mkdir /tt # create tt folder
 export TMP="/tt" # change tmp folder to /tt (for pip)
 export TMPDIR="/tt"
 export XDG_CACHE_HOME="/tt"
+export PATH=$PATH:/opt/bin
 pip --cache-dir=/tt install opencv-python
