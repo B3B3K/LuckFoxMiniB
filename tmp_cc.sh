086f8b869016f0fbf1af25d1ef949c6d9c4355bc
Osman Çelik <76453193+B3B3K@users.noreply.github.com>
2025-01-21 19:55:32 +0300

    reconfigured

--- tmp_cc.sh
+++ tmp_cc.sh
@@ -3,4 +3,6 @@ export TMP="/tt" # change tmp folder to /tt (for pip)
 export TMPDIR="/tt"
 export XDG_CACHE_HOME="/tt"
 export PATH=$PATH:/opt/bin
+export CC=/opt/bin/arm-openwrt-linux-gnueabi-gcc-8.4.0
+export CXX=/opt/bin/arm-openwrt-linux-gnueabi-g++
 pip --cache-dir=/tt install opencv-python
