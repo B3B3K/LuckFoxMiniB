eec91cc28749eccbc33bed254674c3753688c55a
Osman Çelik <76453193+B3B3K@users.noreply.github.com>
2025-01-24 16:17:00 +0300

    .

--- tmp_cc.sh
+++ tmp_cc.sh
@@ -2,7 +2,6 @@ mkdir /tt # create tt folder
 export TMP="/tt" # change tmp folder to /tt (for pip)
 export TMPDIR="/tt"
 export XDG_CACHE_HOME="/tt"
-export PATH=$PATH:/opt/bin
 export CC=/opt/bin/arm-openwrt-linux-gnueabi-gcc-8.4.0
 export CXX=/opt/bin/arm-openwrt-linux-gnueabi-g++
 pip --cache-dir=/tt install opencv-python
